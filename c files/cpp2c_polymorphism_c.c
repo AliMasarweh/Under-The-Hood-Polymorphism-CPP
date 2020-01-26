4ccaae381ab405d36bfb1b91864492230bbe9d24
Ali Masarwe <33619630+AliMasarweh@users.noreply.github.com>
2020-01-26 10:12:05 +0200

    Converted the main to c, only tested compilation & syntax

--- c files/cpp2c_polymorphism_c.c
+++ c files/cpp2c_polymorphism_c.c
@@ -1,18 +1,30 @@
-#include "cpp2c_polymorphism_defs.h"
+//
+// Created by ali-masa on 1/25/20.
+//
 
-
-
+#include <stdlib.h>
+#include "cpp2c_polymorphism_defs_c.h"
+#include "cpp2c_polymorphism_virtual_tables_c.h"
 
 
 void doPrePostFixer()
 {
     printf("\n--- start doPrePostFixer() ---\n\n");
 
-    PrePostFixer angleBrackets("<<< ", " >>>");
-    angleBrackets.print("Hello World!");
-    angleBrackets.print(-777);
-    angleBrackets.print(350, '#');
-    angleBrackets.print(static_cast<long int>(3.14));
+    PrePostFixer angleBrackets;
+    PrePostFixer__PrePostFixer_s_s_2(&angleBrackets, "<<< ", " >>>");
+
+    char defaultSymbol = ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*) &angleBrackets)->vtpntr)
+            ->getDefaultSymbol(&angleBrackets);
+
+    ((struct TEXT_FORMATTER__VTABLE*)((TextFormatter*)&angleBrackets)->vtpntr)
+            ->print("Hello World!");
+    ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*)&angleBrackets)->vtpntr)
+            ->print_l_c(&angleBrackets, -777, defaultSymbol);
+    ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*)&angleBrackets)->vtpntr)
+            ->print_l_c(&angleBrackets, 350, '#');
+    ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*)&angleBrackets)->vtpntr)
+            ->print_l_c(&angleBrackets, (long int)3.14, defaultSymbol);
 
     printf("\n--- end doPrePostFixer() ---\n\n");
 }
@@ -21,10 +33,19 @@ void doPrePostDollarFixer()
 {
     printf("\n--- start doPrePostDollarFixer() ---\n\n");
 
-    PrePostDollarFixer asterisks("***** ", " *****");
-    asterisks.print(-777);
-    asterisks.print(350, '#');
-    asterisks.print(3.14f);
+    PrePostDollarFixer asterisks;
+    PrePostDollarFixer__PrePostDollarFixer_s_s_2(&asterisks,
+            "***** ", " *****");
+
+    char defaultSymbol = ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*) &asterisks)->vtpntr)
+            ->getDefaultSymbol(&asterisks);
+
+    ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*)&asterisks)->vtpntr)
+            ->print_l_c(&asterisks, -777, defaultSymbol);
+    ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*)&asterisks)->vtpntr)
+            ->print_l_c(&asterisks, 350, '#');
+    ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*)&asterisks)->vtpntr)
+            ->print_l_c(&asterisks, (long int)3.14, defaultSymbol);
 
     printf("\n--- end doPrePostDollarFixer() ---\n\n");
 }
@@ -34,12 +55,12 @@ void doPrePostChecker()
     printf("\n--- start doPrePostChecker() ---\n\n");
 
     PrePostChecker check;
-    check.printThisSymbolUsingFunc();
-    check.printThisSymbolDirectly();
-    check.printDollarSymbolByCastDirectly();
-    check.printDollarSymbolByScopeDirectly();
-    check.printDollarSymbolByCastUsingFunc();
-    check.printDollarSymbolByScopeUsingFunc();
+    kPrePostChecker__printThisSymbolUsingFunc_0(&check);
+    kPrePostChecker__printThisSymbolDirectly_0(&check);
+    kPrePostChecker__printDollarSymbolByCastDirectly_0(&check);
+    kPrePostChecker__printDollarSymbolByScopeDirectly_0(&check);
+    kPrePostChecker__printDollarSymbolByCastUsingFunc_0(&check);
+    kPrePostChecker__printDollarSymbolByScopeUsingFunc_0(&check);
 
     printf("\n--- end doPrePostChecker() ---\n\n");
 }
@@ -48,32 +69,58 @@ void doPrePostFloatDollarFixer()
 {
     printf("\n--- start doPrePostFloatDollarFixer() ---\n\n");
 
-    PrePostFloatDollarFixer hashes("### ", " ###");
-    hashes.print(-777);
-    hashes.print(350, '#');
-    hashes.print(3.14f);
+    PrePostFloatDollarFixer hashes;
+    PrePostFloatDollarFixer__PrePostFloatDollarFixer_s_s_2(&hashes,
+            "### ", " ###");
 
-    PrePostDollarFixer hashes2(hashes);
-    hashes2.print(7.5);
-    hashes2.print(100);
+    char defaultSymbol = ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*) &hashes)->vtpntr)
+            ->getDefaultSymbol(&hashes);
+
+    ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*)&hashes)->vtpntr)
+            ->print_l_c(&hashes, -777, defaultSymbol);
+    ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*)&hashes)->vtpntr)
+            ->print_l_c(&hashes, 350, '#');
+    ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*)&hashes)->vtpntr)
+            ->print_l_c(&hashes, (long int)3.14, defaultSymbol);
+
+    PrePostDollarFixer hashes2;
+    PrePostDollarFixer__PrePostDollarFixer_kPrePostHashFixerkP_1(&hashes2, &hashes);
+
+    char defaultSymbol2 = ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*) &hashes2)->vtpntr)
+            ->getDefaultSymbol(&hashes2);
+
+    ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*)&hashes)->vtpntr)
+            ->print_l_c(&hashes, 7.5, defaultSymbol);
+    ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*)&hashes)->vtpntr)
+            ->print_l_c(&hashes, 100, defaultSymbol);
 
     printf("\n--- start doPrePostFloatDollarFixer() ---\n\n");
 }
 
-void runAsPrePostFixerRef(const PrePostFixer& pp)
+void runAsPrePostFixerRef(const PrePostFixer* const pp)
 {
     printf("\n--- start runAsPrePostFixerRef() ---\n\n");
 
-    pp.print(123);
+    char defaultSymbol2 = ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*) pp)->vtpntr)
+            ->getDefaultSymbol(pp);
+
+    ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*)pp)->vtpntr)
+            ->print_l_c(pp, 123, defaultSymbol2);
+
 
     printf("\n--- end runAsPrePostFixerRef() ---\n\n");
 }
 
-void runAsPrePostDollarFixerRef(const PrePostDollarFixer& pp)
+void runAsPrePostDollarFixerRef(const PrePostDollarFixer* const pp)
 {
     printf("\n--- start runAsPrePostDollarFixerRef() ---\n\n");
 
-    pp.print(123);
+    char defaultSymbol2 = ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*) pp)->vtpntr)
+            ->getDefaultSymbol(pp);
+
+    ((struct PRE_POST_DOLLAR_FIXER__VTABLE*)((TextFormatter*)pp)->vtpntr)
+            ->print_l_c(pp, 123, defaultSymbol2);
+
 
     printf("\n--- end runAsPrePostDollarFixerRef() ---\n\n");
 }
@@ -82,16 +129,24 @@ void runAsPrePostDollarFixerObj(const PrePostDollarFixer pp)
 {
     printf("\n--- start runAsPrePostDollarFixerObj() ---\n\n");
 
-    pp.print(123);
+    char defaultSymbol2 = ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*) &pp)->vtpntr)
+            ->getDefaultSymbol(&pp);
+
+    ((struct PRE_POST_DOLLAR_FIXER__VTABLE*)((TextFormatter*)&pp)->vtpntr)
+            ->print_l_c(&pp, 123, defaultSymbol2);
 
     printf("\n--- end runAsPrePostDollarFixerObj() ---\n\n");
 }
 
-void runAsPrePostHashFixerRef(const PrePostHashFixer& pp)
+void runAsPrePostHashFixerRef(const PrePostHashFixer* const pp)
 {
     printf("\n--- start runAsPrePostHashFixerRef() ---\n\n");
 
-    pp.print(123);
+    char defaultSymbol2 = ((struct PRE_POST_FIXER__VTABLE*)((TextFormatter*) &pp)->vtpntr)
+            ->getDefaultSymbol(&pp);
+
+    ((struct PRE_POST_DOLLAR_FIXER__VTABLE*)((TextFormatter*)&pp)->vtpntr)
+    ->print_l_c(&pp, 123, defaultSymbol2);
 
     printf("\n--- end runAsPrePostHashFixerRef() ---\n\n");
 }
@@ -100,15 +155,18 @@ void doMultiplier()
 {
     printf("\n--- start doMultiplier() ---\n\n");
 
-    Multiplier m1(3);
-    Multiplier m2 = 5;
+    Multiplier m1;
+    Multiplier__Multiplier_i_1(&m1, 3);
+    Multiplier m2;
+    Multiplier__Multiplier_i_1(&m2, 5);
     Multiplier m3 = m1;
-    Multiplier m4(m2);
+    Multiplier m4;
+    Multiplier__Multiplier_i_1(&m4, m2.times);
 
-    m1.print("abc ");
-    m2.print("abc ");
-    m3.print("abc ");
-    m4.print("abc ");
+    Multiplier__print(&m1, "abc ");
+    Multiplier__print(&m2, "abc ");
+    Multiplier__print(&m3, "abc ");
+    Multiplier__print(&m4, "abc ");
 
     printf("\n--- start doMultiplier() ---\n\n");
 }
@@ -116,16 +174,20 @@ void doMultiplier()
 void doFormatterArray()
 {
     printf("\n--- start doFormatterArray() ---\n\n");
-
+    DefaultTextFormatter formatter1, formatter2, formatter3;
+    PrePostDollarFixer__PrePostDollarFixer_s_s_2(&formatter1, "!!! ", " !!!");
+    Multiplier__Multiplier_i_1(&formatter2, 4);
+    PrePostChecker__PrePostChecker_0(&formatter3);
     DefaultTextFormatter formatters[] =
-    {
-        PrePostDollarFixer("!!! ", " !!!"),
-        Multiplier(4),
-        PrePostChecker()
-    };
+            {
+                    formatter1,
+                    formatter2,
+                    formatter3
+            };
 
     for (int i = 0; i < 3; ++i)
-        formatters[i].print("Hello World!");
+        ((struct TEXT_FORMATTER__VTABLE*)((TextFormatter*)&formatters[i])->vtpntr)
+                ->print("Hello World!");
 
     printf("\n--- end doFormatterArray() ---\n\n");
 }
@@ -134,18 +196,29 @@ void doFormatterPtrs()
 {
     printf("\n--- start doFormatterPtrs() ---\n\n");
 
+    DefaultTextFormatter *formatter1, *formatter2, *formatter3;
+
+    formatter1 = (DefaultTextFormatter*) malloc(sizeof(PrePostDollarFixer));
+    formatter2 = (DefaultTextFormatter*) malloc(sizeof(Multiplier));
+    formatter3 = (DefaultTextFormatter*) malloc(sizeof(PrePostChecker));
+
+    PrePostDollarFixer__PrePostDollarFixer_s_s_2(formatter1, "!!! ", " !!!");
+    Multiplier__Multiplier_i_1(formatter2, 4);
+    PrePostChecker__PrePostChecker_0(formatter3);
+
     DefaultTextFormatter* pfmt[] =
-    {
-        new PrePostDollarFixer("!!! ", " !!!"),
-        new Multiplier(4),
-        new PrePostChecker()
-    };
+            {
+                    formatter1,
+                    formatter2,
+                    formatter3
+            };
 
     for (int i = 0; i < 3; ++i)
-        pfmt[i]->print("Hello World!");
+        ((struct TEXT_FORMATTER__VTABLE*)((TextFormatter*)&pfmt[i])->vtpntr)
+                ->print("Hello World!");
 
     for (int i = 2; i >= 0; --i)
-        delete pfmt[i];
+        free(&pfmt[i]);
 
     printf("\n--- end doFormatterPtrs() ---\n\n");
 }
@@ -155,11 +228,13 @@ void doFormatterDynamicArray()
     printf("\n--- start doFormatterDynamicArray() ---\n\n");
 
     DefaultTextFormatter* formatters = generateFormatterArray();
-    
-    for (int i = 0; i < 3; ++i)
-        formatters[i].print("Hello World!");
 
-    delete[] formatters;
+    for (int i = 0; i < 3; ++i)
+        ((struct TEXT_FORMATTER__VTABLE*)((TextFormatter*)&formatters[i])->vtpntr)
+                ->print("Hello World!");
+
+    for (int i = 2; i >= 0; --i)
+        free(&formatters[i]);
 
     printf("\n--- start doFormatterDynamicArray() ---\n\n");
 }
@@ -174,10 +249,11 @@ int main()
     doPrePostChecker();
 
     PrePostHashFixer hfix;
-    runAsPrePostFixerRef(hfix);
-    runAsPrePostDollarFixerRef(hfix);
-    runAsPrePostDollarFixerObj(hfix);
-    runAsPrePostHashFixerRef(hfix);
+    PrePostHashFixer__PrePostHashFixer_i_1(&hfix, 4);
+    runAsPrePostFixerRef(&hfix);
+    runAsPrePostDollarFixerRef(&hfix);
+    runAsPrePostDollarFixerObj(*((PrePostDollarFixer*) &hfix));
+    runAsPrePostHashFixerRef(&hfix);
 
     doMultiplier();
 
@@ -189,6 +265,8 @@ int main()
 
     return 0;
 }
+
+
 
 
 
